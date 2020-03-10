23c88c2ded4884d3daac2b4f372000800bacac18
enoch ajagbe <enoch.ajagbe1@gmail.com>
2020-03-10 05:10:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+var conversation = `Let's talk about ${topic}`;
+>>>>>>> e97229c0010a13530e71bd347f7bf5d9da5efce3
